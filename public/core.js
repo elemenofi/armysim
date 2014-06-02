6463374b5d9264d563a0623b1fbf96c3c6b3ca78
elemenofi <manuel.acobo@gmail.com>
2014-06-02 00:37:50 -0300

    imprvs

--- public/core.js
+++ public/core.js
@@ -4,20 +4,20 @@
 var army = angular.module('army', []);
 
 function armyController($scope, $http) {
-	var no_errors = true;
-	function getArmyData () {
-		$http
-			.get('/army')
-			.success(function(data) {
-				$scope.army = data;
-				getArmyData();
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-				no_errors = false;
-			});
-	}
-	if (no_errors) {
-		getArmyData();
-	}
+  var no_errors = true;
+  function getArmyData () {
+    $http
+      .get('/army')
+      .success(function(data) {
+        $scope.army = data;
+        getArmyData();
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+        no_errors = false;
+      });
+  }
+  if (no_errors) {
+    getArmyData();
+  }
 }
