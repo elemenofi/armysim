1a1b8372b5c728bfae2add09de5688f81492efda
manuelwirall <manuel@wirallinteractive.com.ar>
2015-05-16 23:26:48 -0700

    test

--- sim/staffManager.js
+++ sim/staffManager.js
@@ -2,6 +2,8 @@ var _ = require('underscore');
 var staffRecruiter = require('./staffRecruiter');
 var unitManager = require('./unitManager');
 
+var staffShadow = [];
+
 var staff = [];
 
 function recruitCaptain (army, unit) {
