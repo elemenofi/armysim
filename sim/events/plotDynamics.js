7f01e2895a40f27d2ec450f2cf60d1309bc67b1e
Manuel Aberg Cobo <manuel.aberg@globant.com>
2015-05-19 11:43:47 -0300

    refactor plot dynamics

--- sim/events/plotDynamics.js
+++ sim/events/plotDynamics.js
@@ -4,7 +4,7 @@ var staffManager = require('../staff/staffManager');
 
 function updatePlots(army) {
 
-  function forceRetire (target, army, message, plotters) {
+  function forceRetire (target, message, plotters) {
     
     var plottersNames = '';
 
@@ -20,7 +20,7 @@ function updatePlots(army) {
 
   };
 
-  function applyPlot (plotters, plotLeader, target, message) {
+  function applyPlot (plotters, target, message) {
     
     if (plotters.length >= 2) {
 
@@ -39,7 +39,7 @@ function updatePlots(army) {
 
       if (target.prestige <= plotPrestige) {
 
-        forceRetire(target, army, message, plotters);
+        forceRetire(target, message, plotters);
 
       };
 
@@ -52,130 +52,56 @@ function updatePlots(army) {
       });
 
     };
+    
   };
 
-  function planDivisionPlots () {
+  function checkPlottingSubUnits (unit, subUnits, plotters) {
 
-    var target = army.commander;
-    var plotters = [];
-
-    _.each(army.divisions, function (division) {
-    
-      if ((division.commander.drift > 500 && target.drift < 500) || (division.commander.drift < 500 && target.drift > 500)) {
-    
-        plotters.push(division.commander);
-        applyPlot(plotters, division.commander, target, "forced to retire");
-    
+    _.each(unit[subUnits], function (subUnit) {
+  
+      if ((subUnit.commander.drift > 500 && unit.commander.drift < 500) || (subUnit.commander.drift < 500 && unit.commander.drift > 500)) {
+  
+        plotters.push(subUnit.commander);
+        applyPlot(plotters, unit.commander, "forced to retire");
+      
       } else {
-    
-        division.commander.plotting = false;
-    
+  
+        subUnit.commander.plotting = false;
+      
       };
-    
-    });
   
+    });
+
   };
 
-  function planBrigadePlots () {
+  function planPlots (units, subUnits) {
 
-    _.each(army.divisions, function (division) {
+    if (units === "army") {
 
       var plotters = [];
 
-      _.each(division.brigades, function(brigade) {
-  
-        if ((brigade.commander.drift > 500 && division.commander.drift < 500) || (brigade.commander.drift < 500 && division.commander.drift > 500)) {
+      checkPlottingSubUnits(units, subUnits, plotters);
 
-          plotters.push(brigade.commander);
-          applyPlot(plotters, brigade.commander, division.commander, "forced to retire");
+    } else {
 
-  
-        } else {
-          brigade.commander.plotting = false;
-  
-        };
-  
+      _.each(army[units], function (unit) {
+      
+        var plotters = [];
+
+        checkPlottingSubUnits(unit, subUnits, plotters);
+
       });
 
-    });
+    };
     
   };
 
-  function planRegimentPlots () {
+  planPlots("army", "divisions");
+  planPlots("divisions", "brigades");
+  planPlots("brigades", "regiments");
+  planPlots("regiments", "companies");
+  planPlots("companies", "battalions");
 
-    _.each(army.brigades, function (brigade) {
-
-      var plotters = [];
-
-      _.each(brigade.regiments, function(regiment) {
-        if ((regiment.commander.drift > 500 && brigade.commander.drift < 500) || (regiment.commander.drift < 500 && brigade.commander.drift > 500)) {
-
-          plotters.push(regiment.commander);
-          applyPlot(plotters, regiment.commander, brigade.commander, "forced to retire");
-
-        } else {
-          regiment.commander.plotting = false;
-        };
-      });
-
-    });
-
-  };
-
-  function planCompanyPlots () {
-  
-    _.each(army.regiments, function (regiment) {
-  
-      var plotters = [];
-  
-      _.each(regiment.companies, function(company) {
-  
-        if ((company.commander.drift > 500 && regiment.commander.drift < 500) || (company.commander.drift < 500 && regiment.commander.drift > 500)) {
-          plotters.push(company.commander);
-          applyPlot(plotters, company.commander, regiment.commander, "forced to retire");
-  
-        } else {
-  
-          company.commander.plotting = false;
-  
-        };
-  
-      });
-  
-    });
-  
-  };
-
-  function planBattalionPlots () {
-    
-    _.each(army.companies, function (company) {
-    
-      var plotters = [];
-      
-      _.each(company.battalions, function(battalion) {
-      
-        if ((battalion.commander.drift > 500 && company.commander.drift < 500) || (battalion.commander.drift < 500 && company.commander.drift > 500)) {
-      
-          plotters.push(battalion.commander);
-          applyPlot(plotters, battalion.commander, company.commander, "forced to retire");
-      
-        } else {
-      
-          battalion.commander.plotting = false;
-      
-        };
-      
-      });
-    
-    });
-  
-  };
-
-  planDivisionPlots();
-  planBrigadePlots();
-  planRegimentPlots();
-  planCompanyPlots();
-  planBattalionPlots();
 };
 
 exports.update = function (army) {
