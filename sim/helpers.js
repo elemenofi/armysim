be6ebc61043de062ae684b9e85ef257e28349a70
Manuel Aberg Cobo <manuel.aberg@globant.com>
2015-05-19 10:48:55 -0300

    remove old files

--- sim/helpers.js
+++
@@ -1,6 +0,0 @@
-var MersenneTwister = require('mersenne-twister');
-var randomNumberGenerator = new MersenneTwister();
-
-exports.randomNumber = function (range) {
-	return  Math.round(randomNumberGenerator.random() * range);
-};
