b2ebf6da2863ee83be7f43c726be6c9cd7740f45
elemenofi <manuel.acobo@gmail.com>
2017-02-02 00:11:33 +0100

    Chau files

--- es6sim/game.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const engine_1 = require("./engine");
-const army_1 = require("./army");
-const keyboard_1 = require("./keyboard");
-window.army = new army_1.default();
-window.army.engine = new engine_1.default(window.army);
-window.army.keyboard = new keyboard_1.default(window.army.engine);
