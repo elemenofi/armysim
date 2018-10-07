e0c9fef255505a2b1011f1715e64af0d064d9b56
elemenofi <manuel.acobo@gmail.com>
2018-10-07 21:34:14 +0200

    formatting

--- src/game/ui/operations.tsx
+++ src/game/ui/operations.tsx
@@ -7,38 +7,38 @@ import { Game } from '../entities/game';
 import { OperationTypes } from '../entities/officerOperations';
 
 export class UIOperations extends React.Component {
-    props: {
-      game: Game,
-      hq: Headquarter,
-      officer: Officer,
-    }
-  
-    constructor (props) {
-      super(props)
-    }
+  props: {
+    game: Game,
+    hq: Headquarter,
+    officer: Officer,
+  }
 
-    select (which: OperationTypes, e) {
-      e.preventDefault()
-      this.props.hq.player.operations.planning = which
-      this.props.game.advance()
-    }
-  
-    render () {
-      const o = this.props.officer
-  
-      if (!o || !this.props.officer.isPlayer) return <div></div>
+  constructor(props) {
+    super(props)
+  }
 
-      const current = <div className='operationsCurrent'>{o.operations.planning}</div>
-  
-      const actions = <ul className='actions operations'>
-        {current}
-        <li><button onClick={(e) => this.select(OperationTypes.combat, e)}>Combat</button></li>
-        <li><button onClick={(e) => this.select(OperationTypes.intelligence, e)}>Intelligence</button></li>
-      </ul>
-  
-      return <div>
-        <hr className='divider'></hr>
-        {actions}
-      </div>
-    }
+  select(which: OperationTypes, e) {
+    e.preventDefault()
+    this.props.hq.player.operations.planning = which
+    this.props.game.advance()
+  }
+
+  render() {
+    const o = this.props.officer
+
+    if (!o || !this.props.officer.isPlayer) return <div></div>
+
+    const current = <div className='operationsCurrent'>{o.operations.planning}</div>
+
+    const actions = <ul className='actions operations'>
+      {current}
+      <li><button onClick={(e) => this.select(OperationTypes.combat, e)}>Combat</button></li>
+      <li><button onClick={(e) => this.select(OperationTypes.intelligence, e)}>Intelligence</button></li>
+    </ul>
+
+    return <div>
+      <hr className='divider'></hr>
+      {actions}
+    </div>
+  }
 }
